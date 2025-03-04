05e4a9b890209c0654fb356c13410c63e9ef8824
abbiebarnhardt <Abigail.barnhardt@cuw.edu>
2025-03-03 20:52:16 -0600

    added submit page

---
+++ app/src/main/java/com/example/myapplication/ui/submit/SubmitFragment.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SubmitFragment extends Fragment {
+
+    private SubmitViewModel mViewModel;
+
+    public static SubmitFragment newInstance() {
+        return new SubmitFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_submit, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(SubmitViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
